e8b51134dae60cb18f13a9e31097b4993ed7c028
ynren1020 <renxx275@umn.edu>
2022-05-31 10:53:01 -0500

    Create DSFS.py

---
+++ DSFS.py
@@ -0,0 +1,19 @@
+# Notes from Data Science from Scratch First Priciples with Python 
+# Week 1
+# Ch.2 A crash course in Python
+# Before you start, create an virtual environment for each project as the different versions of softwares required by different projects may conflict with each other. Install iPython for interactive work.
+# Note1: when write python code, you should always use spaces rather than tabs for idention. 
+
+# Modules
+import re as regrex 
+import matplotlib.pyplot as plt
+from collections import defaultdict, Counter
+
+# Functions 
+def another_double(x):
+"""numeric value times 2 """
+return 2*x
+
+def full_name(first="first-name",last="last-name"):
+  return first+" "+last
+
